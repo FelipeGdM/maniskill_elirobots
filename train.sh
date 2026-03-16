6bdae2210510b8ea2a945f9d3032eaf7b3f7c4ee
Felipe Gomes de Melo <felipegmelo@usp.br>
2026-03-16 15:02:08 +0000

    Rename train script

--- train.sh
+++ train.sh
@@ -6,11 +6,16 @@
 
 python src/maniskill_elirobots/scripts/ppo.py \
     --env_id="PushCubeEc-v1" \
-    --exp-name="state-pushcube-les-goo-4" \
     --num_envs=1024 \
     --update_epochs=8 \
     --num_minibatches=32 \
-    --total_timesteps=2_400_000 \
+    --total_timesteps=4_096_001 \
     --eval_freq=8 \
     --num-steps=50 \
-    --num-eval-steps=100
+    --num-eval-steps=50
+
+    # --checkpoint="runs/PushCubeEc-v1__ppo__1__1773663971/ckpt_5120000.pt" \
+
+    # --checkpoint="runs/PushCubeEc-v1__ppo__1__1773661708/ckpt_1177600.pt" \
+
+    # --total_timesteps=2_048_001 \
