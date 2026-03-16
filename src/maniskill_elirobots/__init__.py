1b2e5667d10f7f68188ac6e23ab28d54ae3323fb
Felipe Gomes de Melo <felipegmelo@usp.br>
2026-03-16 15:01:16 +0000

    Add robots to general __init__ file

--- src/maniskill_elirobots/__init__.py
+++ src/maniskill_elirobots/__init__.py
@@ -1,3 +1,5 @@
+from .robots.dual_ec63 import DualEC63
+from .robots.ec63 import EC63
 from .tasks import PushCubeEcEnv
 
-__all__ = ["PushCubeEcEnv"]
+__all__ = ["EC63", "DualEC63", "PushCubeEcEnv"]
