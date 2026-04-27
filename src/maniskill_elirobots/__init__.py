fa9461c4c6d11651a022f0c1486f10ab569428be
Felipe Gomes de Melo <felipegmelo@usp.br>
2026-04-27 17:42:34 -0300

    Update __init__.py files

--- src/maniskill_elirobots/__init__.py
+++ src/maniskill_elirobots/__init__.py
@@ -1,5 +1,5 @@
 from .robots.dual_ec63 import DualEC63
 from .robots.ec63 import EC63
-from .tasks import PushCubeEcEnv
+from .tasks import FlipCoinEnv, PushCubeEcEnv
 
-__all__ = ["EC63", "DualEC63", "PushCubeEcEnv"]
+__all__ = ["EC63", "DualEC63", "FlipCoinEnv", "PushCubeEcEnv"]
--- src/maniskill_elirobots/tasks/__init__.py
+++ src/maniskill_elirobots/tasks/__init__.py
@@ -1,3 +1,5 @@
+from .flip_coin_ec import FlipCoinEnv
+from .pick_cube_ec import PickCubeEcEnv
 from .push_cube_ec import PushCubeEcEnv
 
-__all__ = ["PushCubeEcEnv"]
+__all__ = ["FlipCoinEnv", "PickCubeEcEnv", "PushCubeEcEnv"]
