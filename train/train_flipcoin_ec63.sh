b1a9d51ea8683a4febbf83c5b6459721ee367e68
Felipe Gomes de Melo <felipegmelo@usp.br>
2026-07-17 18:57:28 +0000

    Train commit

--- train/train_flipcoin_ec63.sh
+++ train/train_flipcoin_ec63.sh
@@ -6,8 +6,21 @@ git commit -am "Train commit"
 
 python src/maniskill_elirobots/trainer/ppo_cleanrl.py \
     --ent_coef=1e-2 \
-    --exp_name=flipcoin-ec63-(date +%s)-(git rev-parse --short HEAD) \
-    --env_id="FlipCoin-v1"
+    --exp_name=flipcoin-ec63-(date +%s)-(git rev-parse --short HEAD)-gamma-090 \
+    --env_id="FlipCoin-v1" \
+    --gamma=0.9
+
+python src/maniskill_elirobots/trainer/ppo_cleanrl.py \
+    --ent_coef=1e-2 \
+    --exp_name=flipcoin-ec63-(date +%s)-(git rev-parse --short HEAD)-gamma-095 \
+    --env_id="FlipCoin-v1" \
+    --gamma=0.95
+
+python src/maniskill_elirobots/trainer/ppo_cleanrl.py \
+    --ent_coef=1e-2 \
+    --exp_name=flipcoin-ec63-(date +%s)-(git rev-parse --short HEAD)-gamma-099 \
+    --env_id="FlipCoin-v1" \
+    --gamma=0.99
 
 # python vendor/ManiSkill/examples/baselines/ppo/ppo.py \
 #     --checkpoint=runs/flipcoin-ec63-1784048581-5f55005/ckpt_8192000.pt \
