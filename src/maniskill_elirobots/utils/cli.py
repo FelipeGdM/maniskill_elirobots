0aaf0f46dbd7f791974af34778e7fdb1b2eeb67f
Felipe Gomes de Melo <felipegmelo@usp.br>
2026-07-17 19:00:20 +0000

    Train commit

--- src/maniskill_elirobots/utils/cli.py
+++ src/maniskill_elirobots/utils/cli.py
@@ -36,7 +36,7 @@ class CliArgs:
     """total timesteps of the experiments"""
     learning_rate: float = 3e-4
     """the learning rate of the optimizer"""
-    num_envs: int = 1024 * 2
+    num_envs: int = 1024
     """the number of parallel environments"""
     num_eval_envs: int = 8
     """the number of parallel evaluation environments"""
